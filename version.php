0adb430ad5b157406798dac3db88712e81380a06
Gopal Sharma <gopal.sharma@trigyn.com>
2025-05-04 11:39:48 +0530

    updated version file.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ungraded_assignments';
-$plugin->version = 2024030600;
+$plugin->version = 2025050400;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->release = '2.0';

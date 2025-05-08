29e4518bc93b9c6463583f85e14feab0f9546511
Gopal Sharma <gopalsharma66@gmail.com>
2025-05-08 09:16:54 +0530

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ungraded_assignments';
-$plugin->version = 2025050801;
+$plugin->version = 2025050701;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '5.0';
+$plugin->release = '3.0';
